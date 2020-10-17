3c83cd1e609ff69024e7d913bf97288a8fd7846f
iqbalaulia <miqbalar12@gmail.com>
2020-10-18 00:36:29 +0700

    [created] API user method delete

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -93,8 +93,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+         $user->delete();
+
+         return response('Deleted', Response::HTTP_OK);
     }
 }
