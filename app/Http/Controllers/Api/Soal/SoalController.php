b4bca9ea04b63f1ac60d801f64ca1c6da37adb05
iqbalaulia <miqbalar12@gmail.com>
2020-10-17 21:25:22 +0700

    [created] API soal method update data

--- app/Http/Controllers/Api/Soal/SoalController.php
+++ app/Http/Controllers/Api/Soal/SoalController.php
@@ -10,11 +10,7 @@ use Illuminate\Http\Response;
 
 class SoalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         // $soal = Soal::all();
@@ -25,22 +21,6 @@ class SoalController extends Controller
 
     } 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $soal = Soal::create($request->all());
@@ -48,46 +28,26 @@ class SoalController extends Controller
         return response(new SoalResource($soal), Response::HTTP_CREATED);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function show(Soal $soal)
     {
         return new SoalResource($soal);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $soal = Soal::find($id);
+
+        // dd($soal);
+
+        $soal->update($request->all());    
+        
+        // dd($request->all());
+
+        return response(new SoalResource($soal), Response::HTTP_CREATED);
+
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
