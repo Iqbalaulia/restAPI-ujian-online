e5d31ede4376b213bd484a82436571553cb74032
iqbalaulia <miqbalar12@gmail.com>
2020-10-17 20:38:54 +0700

    [created] API soal method show berdasarkan id

--- app/Http/Controllers/Api/Soal/SoalController.php
+++ app/Http/Controllers/Api/Soal/SoalController.php
@@ -54,9 +54,9 @@ class SoalController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Soal $soal)
     {
-        //
+        return new SoalResource($soal);
     }
 
     /**
