6748d3773bfa1c703344a1301b67b40f8f709958
iqbalaulia <miqbalar12@gmail.com>
2021-01-22 09:16:56 +0700

    testing2

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,7 @@ Route::post('user/login','Api\User\UserController@login');
 // testing
 Route::get('nilai', 'Api\Nilai\NilaiController@index');
 // testing2
+// testing2
 Route::get('nilai/{android_id}', 'Api\Nilai\NilaiController@show');
 
 Route::get('dashboard', 'Api\Nilai\NilaiController@dashboard');
